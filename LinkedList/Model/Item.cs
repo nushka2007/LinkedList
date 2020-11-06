411cb52f8973aa08ba1b82ea25607ee99975f23e
nushka2007 <73809343+nushka2007@users.noreply.github.com>
2020-11-07 00:28:52 +0800

    Add files via upload

---
+++ LinkedList/Model/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LinkedList
+{
+    public class Item<T>
+    {
+        private T data = default(T);
+        public T Data { get; set; }
+        public Item<T> Next { get; set; }
+        public Item(T data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ LinkedList/Model/LinkedList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace LinkedList
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        private Item<T> _head = null;
+        private Item<T> _tail = null;
+        private int _count = 0;
+        public int Count
+        {
+            get => _count;
+        }
+        public void Add(T data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+            var item = new Item<T>(data);
+            if (_head == null)
+            {
+                _head = item;
+            }
+            else
+            {
+                _tail.Next = item;
+            }
+            _tail = item;
+            _count++;
+        }
+        public void Delete(T data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+            var current = _head;
+            Item<T> previous = null;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                        {
+                            _tail = previous;
+                        }
+                    }
+                    else
+                    {
+                        _head = _head.Next;
+                        if (_head == null)
+                        {
+                            _tail = null;
+                        }
+                    }
+                    _count--;
+                    break;
+                }
+                previous = current;
+                current = current.Next;
+            }
+        }
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = _head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+}
